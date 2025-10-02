d4c5de0932744bde5c7852c922c96aae0e4f20f2
ardneham544 <ardneham.k@gmail.com>
2025-10-03 02:09:24 +0530

    Update ContactUS.jsx

--- src/components/ContactUs/ContactUS.jsx
+++ src/components/ContactUs/ContactUS.jsx
@@ -14,7 +14,7 @@ function ContactUS() {
             <div className="company-info">
               <h3>Our Office</h3>
               <p>19-20,Chandrakant Enclave,Rajpardi, Dist. Bharuch, Gujarat, India</p>
-              <p>Email: CS.IGNER544@GMAIL.COM</p>
+              <p>Email: HELP@PRASANG.CO.IN</p>
               <p>Phone: +91 9316026002</p>
             </div>
 
